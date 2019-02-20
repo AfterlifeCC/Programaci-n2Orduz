dc26bcc281943bad9f61cefa82842ebb30748697
CitlalliC <42586527+AfterlifeCC@users.noreply.github.com>
2019-02-20 10:38:16 -0600

    Add files via upload
    Tarea para el viernes 1ero marzo 4 ejercicios 24 github(part1readme) y 3 de dropbox (pag22)

---
+++ pareseimpares.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+struct numeros
+{
+
+int pares;
+int impar;
+};
+
+
+int main()
+{
+int i;
+	struct numeros num[n];
+	//printf("Introduzca numero del 1 al 10:");
+    	
+	//scanf("%d",&i);
+
+    //if (i%2==0) {
+       //printf("Es par.\n");
+    }
+    //else
+    {
+       //printf("No es par.%\n");
+    }
+strcpy
+
+
+};
+
---
+++ pareseimpares.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+int i=1,2,3,4;
+
+//for (i=0;i<=10;i++);
+//int i;
+//printf("Introduzca numero del 1 al 4:");
+//scanf("%d",&i);
+if (i%2==0) {
+printf("Es par.\n");
+}
+else
+printf("No es par.%\n");
+	
+	
+}
