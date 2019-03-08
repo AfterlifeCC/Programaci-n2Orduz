57bcf42b6d0fcb4838fbf08e3f8352a30ef74249
CitlalliC <42586527+AfterlifeCC@users.noreply.github.com>
2019-03-08 00:03:43 -0600

    Update 15p2.c

--- 15p2.c
+++ 15p2.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-void Func_Cubo(float * numero);
-void Func_Cubo(float * N);
+//void Func_Cubo(float * numero);
+//void Func_Cubo(float * N);
 
 void main(){
 
@@ -9,16 +9,14 @@ void main(){
 
 printf("De un valor: ");
 scanf("%f", &num);
-Func_Cubo(&num);
-Func_Cubo(&N);
+//Func_Cubo(&num);
+//Func_Cubo(&N);
 }
 
-void Func_Cubo(float * numero){
-void Func_Cubo(float * N){
+void Func_Cubo(float * numero, float * N){
+{
  * numero = (* N) * (* numero);
  * numero = (* N) / (* numero);
  printf("El cubo es: %2.2f \n", * numero);
 }
 }
-
-
