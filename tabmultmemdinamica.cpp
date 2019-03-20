3bf7751b1d9e88135f2224ec6048c0258a93a1c4
CitlalliC <42586527+AfterlifeCC@users.noreply.github.com>
2019-03-20 10:58:18 -0600

    Add files via upload

---
+++ tabmultmemdinamica.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main () {
+int i;
+int a;
+int b=i*a;
+
+for (i=0;i<=10;i++)
+{
+scanf("%i", &a);
+printf(" %i x %i = %i \n", i,a,b );
+//printf ("%i \n", i*3);
+}
+return 0;
+}
