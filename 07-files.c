c8367498439cd3845f48facbfa36e06b50c97602
CitlalliC <42586527+AfterlifeCC@users.noreply.github.com>
2019-03-27 09:54:47 -0600

    Update 07-files.c

--- 07-files.c
+++ 07-files.c
@@ -1,11 +1,12 @@
-//
+
 #include<stdio.h>
 int main(){
 
 FILE * fptr1, *fptr2;
 char c1, c2;
 
-
+//Si no se crea el archivo 01.txt la terminal regresa "Error al abrir el archivo original", y si se crea el 01.txt la terminal no regresa
+//nada, pero crea el archivo 02.txt en el que copia 2 caracteres.
 fptr1 = fopen("07-files01.txt","r");
 
  if(fptr1 == NULL)
